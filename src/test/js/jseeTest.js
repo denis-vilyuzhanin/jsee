aa48cd23dc23786d12f223638720f053a124aa53
Denys Vilyuzhanin <dvilyuzhanin@gmail.com>
2013-04-14 12:24:51 -0400

    clean up tests. Make integration JSEE test module

--- src/test/js/jseeTest.js
+++ src/test/js/jseeTest.js
@@ -1,24 +1,5 @@
 
-JSEE_TEST = true
+module('JSEE')
 
 
-function WindowMock() {
-}
 
-
-function DocumentMock() {
-    
-}
-
-windowMock = new WindowMock();
-documentMock = new DocumentMock();
-
-JSEE = JSEE_MODULE(windowMock, documentMock);
-
-
-function assertEqualEvent(actualEvent, expectedEvent) {
-    ok(actualEvent, "actual event");
-    ok(actualEvent.id, "ID was assigned");
-    equal(actualEvent.id, expectedEvent.id, "event ID");
-    equal(actualEvent.message, expectedEvent.message, "event message");
-}
